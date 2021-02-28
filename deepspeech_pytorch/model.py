7e2208f627dd33cd5ed457ed9936775345537716
tohalb <tom_hales@hotmail.com>
2021-02-28 22:10:02 +0000

    Update model.py

--- deepspeech_pytorch/model.py
+++ deepspeech_pytorch/model.py
@@ -77,29 +77,226 @@ class InferenceBatchSoftmax(nn.Module):
             return input_
 
 
-class BatchRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, rnn_type=nn.LSTM, bidirectional=False, batch_norm=True):
-        super(BatchRNN, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.bidirectional = bidirectional
-        self.batch_norm = SequenceWise(nn.BatchNorm1d(input_size)) if batch_norm else None
-        self.rnn = rnn_type(input_size=input_size, hidden_size=hidden_size,
-                            bidirectional=bidirectional, bias=True)
-        self.num_directions = 2 if bidirectional else 1
+#class BatchRNN(nn.Module):
+#    def __init__(self, input_size, hidden_size, rnn_type=nn.LSTM, bidirectional=False, batch_norm=True):
+#        super(BatchRNN, self).__init__()
+#        self.input_size = input_size
+#        self.hidden_size = hidden_size
+#        self.bidirectional = bidirectional
+#        self.batch_norm = SequenceWise(nn.BatchNorm1d(input_size)) if batch_norm else None
+#        self.rnn = rnn_type(input_size=input_size, hidden_size=hidden_size,
+#                            bidirectional=bidirectional, bias=True)
+#        self.num_directions = 2 if bidirectional else 1
 
-    def flatten_parameters(self):
-        self.rnn.flatten_parameters()
+#    def flatten_parameters(self):
+#        self.rnn.flatten_parameters()
 
-    def forward(self, x, output_lengths):
-        if self.batch_norm is not None:
-            x = self.batch_norm(x)
-        x = nn.utils.rnn.pack_padded_sequence(x, output_lengths)
-        x, h = self.rnn(x)
-        x, _ = nn.utils.rnn.pad_packed_sequence(x)
-        if self.bidirectional:
-            x = x.view(x.size(0), x.size(1), 2, -1).sum(2).view(x.size(0), x.size(1), -1)  # (TxNxH*2) -> (TxNxH) by sum
-        return x
+#    def forward(self, x, output_lengths):
+#        if self.batch_norm is not None:
+#            x = self.batch_norm(x)
+#        x = nn.utils.rnn.pack_padded_sequence(x, output_lengths)
+#        x, h = self.rnn(x)
+#        x, _ = nn.utils.rnn.pad_packed_sequence(x)
+#        if self.bidirectional:
+#            x = x.view(x.size(0), x.size(1), 2, -1).sum(2).view(x.size(0), x.size(1), -1)  # (TxNxH*2) -> (TxNxH) by sum
+#        return x
+
+# NOTE:
+# The Code below for the ConvLSTM Cell and Conv LSTM comes from Andrea Palazzi's repository at https://github.com/ndrplz/ConvLSTM_pytorch
+#
+class ConvLSTMCell(nn.Module):
+    def __init__(self, input_dim, hidden_dim, kernel_size, bias):
+        """
+        Initialize ConvLSTM cell.
+        Parameters
+        ----------
+        input_dim: int
+            Number of channels of input tensor.
+        hidden_dim: int
+            Number of channels of hidden state.
+        kernel_size: (int, int)
+            Size of the convolutional kernel.
+        bias: bool
+            Whether or not to add the bias.
+        """
+
+        super(ConvLSTMCell, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.kernel_size = kernel_size
+        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
+        self.bias = bias
+
+        self.conv = nn.Conv2d(in_channels=self.input_dim + self.hidden_dim,
+                              out_channels=4 * self.hidden_dim,
+                              kernel_size=self.kernel_size,
+                              padding=self.padding,
+                              bias=self.bias)
+
+    def forward(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+
+        combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
+
+        combined_conv = self.conv(combined)
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+
+        c_next = f * c_cur + i * g
+        h_next = o * torch.tanh(c_next)
+
+        return h_next, c_next
+
+    def init_hidden(self, batch_size, image_size):
+        height, width = image_size
+        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device),
+                torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device))
+
+# NOTE:
+# The Code below for the ConvLSTM Cell comes from Andrea Palazzi's repository at https://github.com/ndrplz/ConvLSTM_pytorch
+#
+class ConvLSTM(nn.Module):
+    """
+    Parameters:
+        input_dim: Number of channels in input
+        hidden_dim: Number of hidden channels
+        kernel_size: Size of kernel in convolutions
+        num_layers: Number of LSTM layers stacked on each other
+        batch_first: Whether or not dimension 0 is the batch or not
+        bias: Bias or no bias in Convolution
+        return_all_layers: Return the list of computations for all layers
+        Note: Will do same padding.
+    Input:
+        A tensor of size B, T, C, H, W or T, B, C, H, W
+    Output:
+        A tuple of two lists of length num_layers (or length 1 if return_all_layers is False).
+            0 - layer_output_list is the list of lists of length T of each output
+            1 - last_state_list is the list of last states
+                    each element of the list is a tuple (h, c) for hidden state and memory
+    Example:
+        >> x = torch.rand((32, 10, 64, 128, 128))
+        >> convlstm = ConvLSTM(64, 16, 3, 1, True, True, False)
+        >> _, last_states = convlstm(x)
+        >> h = last_states[0][0]  # 0 for layer index, 0 for h index
+    """
+
+    def __init__(self, input_dim, hidden_dim, kernel_size, num_layers,
+                 batch_first=False, bias=True, return_all_layers=False):
+        super(ConvLSTM, self).__init__()
+        #super(BatchRNN, self).__init__()
+
+        self._check_kernel_size_consistency(kernel_size)
+
+        # Make sure that both `kernel_size` and `hidden_dim` are lists having len == num_layers
+        kernel_size = self._extend_for_multilayer(kernel_size, num_layers)
+        hidden_dim = self._extend_for_multilayer(hidden_dim, num_layers)
+        if not len(kernel_size) == len(hidden_dim) == num_layers:
+            raise ValueError('Inconsistent list length.')
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.kernel_size = kernel_size
+        self.num_layers = num_layers
+        self.batch_first = batch_first
+        self.bias = bias
+        self.return_all_layers = return_all_layers
+
+        cell_list = []
+        for i in range(0, self.num_layers):
+            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i - 1]
+
+            cell_list.append(ConvLSTMCell(input_dim=cur_input_dim,
+                                          hidden_dim=self.hidden_dim[i],
+                                          kernel_size=self.kernel_size[i],
+                                          bias=self.bias))
+
+        self.cell_list = nn.ModuleList(cell_list)
+
+    def forward(self, input_tensor, hidden_state=None):
+        """
+        Parameters
+        ----------
+        input_tensor: todo
+            5-D Tensor either of shape (t, b, c, h, w) or (b, t, c, h, w)
+        hidden_state: todo
+            None. todo implement stateful
+        Returns
+        -------
+        last_state_list, layer_output
+        """
+        if not self.batch_first:
+            # (t, b, c, h, w) -> (b, t, c, h, w)
+            #datum_I_cc = torch.tensor(input_tensor)
+            datum_I_cc = input_tensor.dim()
+            print('TENSOR SHAPE',datum_I_cc)
+            input_tensor.unsqueeze_(0)
+            input_tensor.unsqueeze_(0)
+            datum_I_cc = input_tensor.dim()
+            print('NEW TENSOR SHAPE',datum_I_cc)
+            ##print('INPUT TENSOR',input_tensor)
+            #input_tensor = input_tensor.permute(1, 0, 2, 3, 4)
+            ##input_tensor = input_tensor.permute(1, 0, 2)
+
+        b, _, _, h, w = input_tensor.size()
+
+        # Implement stateful ConvLSTM
+        if hidden_state is not None:
+            raise NotImplementedError()
+        else:
+            # Since the init is done in forward. Can send image size here
+            hidden_state = self._init_hidden(batch_size=b,
+                                             image_size=(h, w))
+
+        layer_output_list = []
+        last_state_list = []
+
+        seq_len = input_tensor.size(1)
+        cur_layer_input = input_tensor
+
+        for layer_idx in range(self.num_layers):
+
+            h, c = hidden_state[layer_idx]
+            output_inner = []
+            for t in range(seq_len):
+                h, c = self.cell_list[layer_idx](input_tensor=cur_layer_input[:, t, :, :, :],
+                                                 cur_state=[h, c])
+                output_inner.append(h)
+
+            layer_output = torch.stack(output_inner, dim=1)
+            cur_layer_input = layer_output
+
+            layer_output_list.append(layer_output)
+            last_state_list.append([h, c])
+
+        if not self.return_all_layers:
+            layer_output_list = layer_output_list[-1:]
+            last_state_list = last_state_list[-1:]
+
+        return layer_output_list, last_state_list
+
+    def _init_hidden(self, batch_size, image_size):
+        init_states = []
+        for i in range(self.num_layers):
+            init_states.append(self.cell_list[i].init_hidden(batch_size, image_size))
+        return init_states
+
+    @staticmethod
+    def _check_kernel_size_consistency(kernel_size):
+        if not (isinstance(kernel_size, tuple) or
+                (isinstance(kernel_size, list) and all([isinstance(elem, tuple) for elem in kernel_size]))):
+            raise ValueError('`kernel_size` must be tuple or list of tuples')
+
+    @staticmethod
+    def _extend_for_multilayer(param, num_layers):
+        if not isinstance(param, list):
+            param = [param] * num_layers
+        return param
+
 
 
 class Lookahead(nn.Module):
@@ -168,23 +365,43 @@ class DeepSpeech(pl.LightningModule):
         rnn_input_size = int(math.floor(rnn_input_size + 2 * 10 - 21) / 2 + 1)
         rnn_input_size *= 32
 
-        self.rnns = nn.Sequential(
-            BatchRNN(
-                input_size=rnn_input_size,
-                hidden_size=self.model_cfg.hidden_size,
-                rnn_type=self.model_cfg.rnn_type.value,
-                bidirectional=self.bidirectional,
-                batch_norm=False
-            ),
-            *(
-                BatchRNN(
-                    input_size=self.model_cfg.hidden_size,
-                    hidden_size=self.model_cfg.hidden_size,
-                    rnn_type=self.model_cfg.rnn_type.value,
-                    bidirectional=self.bidirectional
-                ) for x in range(self.model_cfg.hidden_layers - 1)
-            )
+        #self.rnns = nn.Sequential(
+        #    BatchRNN(
+        #        input_size=rnn_input_size,
+        #        hidden_size=self.model_cfg.hidden_size,
+        #        rnn_type=self.model_cfg.rnn_type.value,
+        #        bidirectional=self.bidirectional,
+        #        batch_norm=False
+        #    ),
+        #    *(
+        #        BatchRNN(
+        #            input_size=self.model_cfg.hidden_size,
+        #            hidden_size=self.model_cfg.hidden_size,
+        #            rnn_type=self.model_cfg.rnn_type.value,
+        #            bidirectional=self.bidirectional
+        #        ) for x in range(self.model_cfg.hidden_layers - 1)
+        #    )
+        #)
+        self.cnns = nn.Sequential(
+            ConvLSTM(
+                input_dim = 5,
+                hidden_dim=self.model_cfg.hidden_size,
+                #hidden_dim = 1312,
+                kernel_size = (3,3),
+                bias = False,
+                num_layers=2
+            )#,
+            #*(
+            #    ConvLSTM(
+            #        input_dim = 5,
+            #        hidden_dim=self.model_cfg.hidden_size,
+            #        kernel_size = (3,3),
+            #        bias = False,
+            #        num_layers=2
+            #    )
+            #)
         )
+
 
         self.lookahead = nn.Sequential(
             # consider adding batch norm?
@@ -220,8 +437,8 @@ class DeepSpeech(pl.LightningModule):
         x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # Collapse feature dimension
         x = x.transpose(1, 2).transpose(0, 1).contiguous()  # TxNxH
 
-        for rnn in self.rnns:
-            x = rnn(x, output_lengths)
+        ##for rnn in self.rnns:
+        ##    x = rnn(x, output_lengths)
 
         if not self.bidirectional:  # no need for lookahead layer in bidirectional
             x = self.lookahead(x)
